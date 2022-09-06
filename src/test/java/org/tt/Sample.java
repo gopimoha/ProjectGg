e1138493de4bb3b55f2fb2e9d580eaf92a3ec557
gopimoha <gopurasan05@gmail.com>
2022-09-06 14:40:02 +0530

    project structure

---
+++ src/test/java/org/tt/Sample.java
@@ -0,0 +1,36 @@
+package org.tt;
+
+public class Sample {
+	
+	
+	public void syat() {
+		
+       System.out.println("test1");
+       System.out.println("test2");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
+
+
+
+
+
+
+
